2c8b23e07291db1d0b83ca3bc09ae013555653a8
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-10-27 23:00:53 +0800

    修改三個頁面的路徑-陳欣妤

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('首頁/',['as'=>'home.index','uses'=> 'HomeController@index']);
+Route::get('/',['as'=>'home.index','uses'=> 'HomeController@index']);
 
-Route::get('關於本站about/',['as'=>'about.index','uses'=> 'AboutController@index']);
+Route::get('/about',['as'=>'about.index','uses'=> 'AboutController@index']);
 
-Route::get('最新消息/news',['as'=>'news.index','uses'=> 'NewsController@index']);
+Route::get('/news',['as'=>'news.index','uses'=> 'NewsController@index']);
